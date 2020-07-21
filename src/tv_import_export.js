9d73c064d8ff4492792febb9df59c069bea2031b
Liam Wilks <liam@liamwilks.onmicrosoft.com>
2020-07-21 20:44:19 +1000

    Fixed Tradingview import\export

--- src/tv_import_export.js
+++ src/tv_import_export.js
@@ -1,14 +1,15 @@
 var fs = require('fs');
 
 var ab_to_tv = async function (filepath) {
-    let doc = fs.readFileSync('C:\\Users\\liamw\\source\\repos\\ab-subee-list-automation\\lists\\SB-Setups.tls', 'utf8')
-    doc = doc.replace(/(\S+)(?=\.)\.(.*)(\n)/g, "$2:$1,").slice(0, -1)
+    let doc = fs.readFileSync(filepath, 'utf8')
+    doc = doc.replace(/(\w+)(?=\.)\.(\w+)/g, "$2:$1")
+    doc = doc.replace(/(\S+)\s+/g, "$1,")
     doc = doc.replace(/AU:/g,"ASX:")
     return doc;
 }
 
 var tv_to_ab = async function (filepath) {
-    let doc = fs.readFileSync('C:\\Users\\liamw\\source\\repos\\ab-subee-list-automation\\lists\\test.tv', 'utf8')
+    let doc = fs.readFileSync(filepath, 'utf8')
     doc = doc.replace(/(\w+)(?=\:)\:(\w+)/g, "$2.$1")
     doc = doc.replace(/\,/g,"\n")
     doc = doc.replace(/^\s*[\r\n]/gm, '');
